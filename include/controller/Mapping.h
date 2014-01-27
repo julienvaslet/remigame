ce7f6e59e0596524172e7c7e2c63d658e6e702c6
Julien Vaslet <julien.vaslet@gmail.com>
2014-01-27 13:27:40 +0100

    Create Mapping.h

---
+++ include/controller/Mapping.h
@@ -0,0 +1,48 @@
+#ifndef	_CONTROLLER_MAPPING_H
+#define _CONTROLLER_MAPPING_H	1
+
+#ifdef DEBUG0
+#include <iostream>
+#endif
+
+#include <map>
+#include <string>
+using namespace std;
+
+namespace controller
+{
+	class Mapping
+	{
+		public:
+			// enum class Button : short int ?
+			enum Button
+			{
+				BTNUP,
+				BTNRIGHT,
+				BTNDOWN,
+				BTNLEFT,
+				LT1,
+				RT1,
+				LT2,
+				RT2,
+				LT3,
+				RT3,
+				SELECT,
+				START,
+				AXH,
+				AXV
+			};
+			
+		protected:
+			map<short int value, Button> mapping;
+		
+		public:
+			Mapping( const string& filename );
+			Mapping( const char * filename );
+			~Mapping();
+			
+			Button getButton( short int value );
+	};
+}
+
+#endif
