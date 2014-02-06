97ef6c598ad3c80697df6c34e5da8694043473d1
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-06 14:18:57 +0100

    Update Player.cpp

--- source/game/Player.cpp
+++ source/game/Player.cpp
@@ -12,14 +12,39 @@ namespace game
 	Player::Player( const char * name ) : controller(NULL)
 	{
 		this->name = string(name);
+		
+		#ifdef DEBUG0
+		cout << "[Player: " << this->name << "] Initialized." << endl;
+		#endif
 	}
 	
 	Player::~Player()
 	{
+		#ifdef DEBUG0
+		cout << "[Player: " << this->name << "] Destroyed." << endl;
+		#endif
 	}
 	
 	void Player::handleEvent( Mapping::Button button, short int value )
 	{
 		
+	}
+	
+	void Player::setController( Controller * controller )
+	{
+		this->controller = controller;
+		
+		if( this->controller != NULL )
+		{
+			#ifdef DEBUG0
+			cout << "[Player: " << this->name << "] Linked to Controller#" << this->controller->getId() << "." << endl;
+			#endif
+		}
+		else
+		{
+			#ifdef DEBUG0
+			cout << "[Player: " << this->name << "] Detached from controller." << endl;
+			#endif	
+		}
 	}
 }
