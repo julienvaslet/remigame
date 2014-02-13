4ddcbeec850ab73fa32c2935a47204566c1756f8
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-13 12:39:55 +0100

    Create Box.h

---
+++ include/graphics/Box.h
@@ -0,0 +1,31 @@
+#ifndef __GRAPHICS_BOX_H
+#define __GRAPHICS_BOX_H	1
+
+#include <graphics/Point.h>
+
+namespace graphics
+{
+	class Box
+	{
+		protected:
+			Point origin;
+			int width;
+			int height;
+			
+		public:
+			Box();
+			Box( const Point * point, int width, int height );
+			Box( int x, int y, int width, int height );
+			~Box();
+			
+			Point * getOrigin();
+			int getWidth() const;
+			int getHeight() const;
+			
+			void setWidth( int width );
+			void setHeight( int height );
+			void resize( int width, int height );
+	};
+}
+
+#endif
