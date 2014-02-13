a895586b25ff9b95f1fc3232979455a40721f5cd
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-13 12:22:22 +0100

    Create Point.cpp

---
+++ source/graphics/Point.cpp
@@ -0,0 +1,48 @@
+#include <graphics/Point.h>
+
+namespace graphics
+{
+	Point::Point() : x(0), y(0)
+	{
+	}
+	
+	Point::Point( int x, int y ) : x(x), y(y)
+	{
+	}
+	
+	Point::~Point()
+	{
+	}
+	
+	int Point::getX()
+	{
+		return this->x;
+	}
+	
+	int Point::getY()
+	{
+		return this->y;
+	}
+	
+	void Point::setX( int x )
+	{
+		this->x = x;
+	}
+	
+	void Point::setY( int y )
+	{
+		this->y = y;
+	}
+	
+	void Point::move( int x, int y )
+	{
+		this->x = x;
+		this->y = y;
+	}
+	
+	void Point::moveBy( int dx, int dy )
+	{
+		this->x += dx;
+		this->y += dy;
+	}
+}
