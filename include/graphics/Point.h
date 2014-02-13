d7937b0c936df846712e82fe08b835426522ae69
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-13 12:40:12 +0100

    Update Point.h

--- include/graphics/Point.h
+++ include/graphics/Point.h
@@ -14,8 +14,8 @@ namespace graphics
 			Point( int x, int y );
 			~Point();
 			
-			int getX();
-			int getY();
+			int getX() const;
+			int getY() const;
 			void setX( int x );
 			void setY( int y );
 			void move( int x, int y );
