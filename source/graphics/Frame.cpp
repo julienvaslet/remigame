0a5e30a48db5912c49897731246e517d1946b3ea
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-13 14:14:12 +0100

    Update Frame.cpp

--- source/graphics/Frame.cpp
+++ source/graphics/Frame.cpp
@@ -14,6 +14,8 @@ namespace graphics
 	
 	Frame::Frame( const Frame& frame ) : box(frame.box), anchor(frame.anchor)
 	{
+		// TODO : Does pointers are mandatory, why don't use reference and exception ?
+		
 		// Import bounding boxes
 		for( vector<Box *>::iterator it = frame.boundingBoxes.begin() ; it != frame.boundingBoxes.end() ; it++ )
 			this->boundingBoxes.push_back( new Box( **it ) );
@@ -50,6 +52,36 @@ namespace graphics
 	Point& Frame::getAnchor()
 	{
 		return this->anchor;
+	}
+	
+	int getBoundingBoxesCount() const
+	{
+		return this->boundingBoxes.size();
+	}
+	
+	int getAttackAreasCount() const
+	{
+		return this->attackAreas.size();
+	}
+	
+	int getDefenceAreasCount() const
+	{
+		return this->defenceAreas.size();
+	}
+	
+	const Box * getBoundingBox( int index ) const
+	{
+		return this->boundingBoxes.at( index );
+	}
+	
+	const Box * getAttackArea( int index ) const
+	{
+		return this->attackAreas.at( index );
+	}
+	
+	const Box * getDefenceArea( int index ) const
+	{
+		return this->defenceAreas.at( index );
 	}
 	
 	void Frame::addBoundingBox( const Box& box )
