727113ec321ecafb485b2744e78a5277ddc1da13
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-24 12:21:34 +0100

    Update Point.h

--- include/graphics/Point.h
+++ include/graphics/Point.h
@@ -21,6 +21,9 @@ namespace graphics
 			void setY( int y );
 			void move( int x, int y );
 			void moveBy( int dx, int dy );
+			
+			void render( unsigned int size = 0 );
+			void render( unsigned int width, unsigned int height );
 	};
 }
 
