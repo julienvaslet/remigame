0e779574b9aa96383dec1fdceab51732272e2569
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-25 13:08:03 +0100

    Update Element.h

--- include/ui/Element.h
+++ include/ui/Element.h
@@ -2,6 +2,7 @@
 #define __UI_ELEMENT_H  1
 
 #include <graphics/Renderable.h>
+#include <graphics/Box.h>
 #include <map>
 #include <string>
 
@@ -15,12 +16,14 @@ namespace ui
 	class Element : graphics::Renderable
 	{
 		protected:
+			Box box;
 			map<string, vector<Event> > events;
 			
 		public:
 			Element();
 			virtual ~Element();
 			
+			Box& getBox();
 			void addEventHandler( const string& event, Event callback );
 			void trigger( const string& event );
 			virtual void render( unsigned int ticks ) = 0;
