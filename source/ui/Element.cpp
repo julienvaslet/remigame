11f2d7837e6a61314e5fac161e2472310936b3ef
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-25 12:43:35 +0100

    Update Element.cpp

--- source/ui/Element.cpp
+++ source/ui/Element.cpp
@@ -10,19 +10,37 @@ namespace ui
 	{
 	}
 	
-	void Element::setEventHandler( const string& event, Event callback )
+	void Element::addEventHandler( const string& event, Event callback )
 	{
-		this->events[event] = callback;
+		map<string, vector<Event> >::iterator it = this->events.find( event );
+		
+		if( it == this->events.end() )
+		{
+			vector<Event> evts;
+			evts.push_back( callback );
+			this->events[event] = evts;
+		}
+		else
+			this->events[event].push_back( callback );
 	}
 
 	void Element::trigger( const string& event )
 	{
-		map<string, Event>::iterator it = this->events.find( event );
+		map<string, vector<Event> >::iterator it = this->events.find( event );
 		
-		if( it != this->events.end() && it->second != NULL )
+		if( it != this->events.end() )
 		{
-			// This could trigger a bad behavior if events are badly setted.
-			(it->second)( this );
+			for( vector<Event>::reverse_iterator itEvent = it->second.rbegin() ; itEvent != it->second.rend() ; itEvent++ )
+			{
+				if( *itEvent != NULL )
+				{
+					// This could trigger a bad behavior if events are badly setted.
+					if( (*itEvent)( this ) == false )
+						break;
+				}
+				else
+					break;
+			}
 		}
 	}
 }
