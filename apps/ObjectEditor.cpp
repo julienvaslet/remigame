93ae0200cb3992ec81dee7ce7f995ffb2eefd11a
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-26 13:20:06 +0100

    Update ObjectEditor.cpp

--- apps/ObjectEditor.cpp
+++ apps/ObjectEditor.cpp
@@ -3,9 +3,13 @@
 
 #include <graphics/Screen.h>
 #include <graphics/Font.h>
+#include <graphics/Box.h>
+#include <graphics/Color.h>
 
 #include <ui/UserInterface.h>
 #include <ui/Button.h>
+
+#include <vector>
 
 using namespace graphics;
 using namespace ui;
@@ -13,7 +17,7 @@ using namespace std;
 
 int main( int argc, char ** argv )
 {
-	if( !Screen::initialize( "Object Editor" ) )
+	if( !Screen::initialize( "Object Editor", 1024, 768, true ) )
 	{
 		cout << "Unable to initialize screen. Exiting." << endl;
 		return 1;
@@ -31,8 +35,19 @@ int main( int argc, char ** argv )
 	unsigned int lastDrawTicks = 0;
 	
 	UserInterface ui;
-	ui.addElement( "myBtn", new Button( "font0", "Message d'essai" ) );
-	ui.getElement( "myBtn" )->getBox().getOrigin().move( 10, 10 );
+	
+	vector<string> panelButtons;
+	Box actionPanel( Screen::get()->getWidth() - 150, 0, 150, Screen::get()->getHeight() );
+	
+	ui.addElement( "btn_load", new Button( "font0", "Load" ) );
+	ui.getElement( "btn_load" )->getBox().setWidth( 80 );
+	ui.getElement( "btn_load" ).getOrigin().move( Screen::get()->getWidth() - ui.getElement( "btn_load" )->getBox().getWidth() - 10, 10 );
+	panelButtons.push_back( "btn_load" );
+	
+	ui.addElement( "btn_save", new Button( "font0", "Save" ) );
+	ui.getElement( "btn_save" )->getBox().setWidth( 80 );
+	ui.getElement( "btn_save" ).getOrigin().move( Screen::get()->getWidth() - ui.getElement( "btn_save" )->getBox().getWidth() - 10, 40 );
+	panelButtons.push_back( "btn_save" );
 
 	while( running )
 	{
@@ -43,6 +58,19 @@ int main( int argc, char ** argv )
 				case SDL_QUIT:
 				{
 					running = false;
+					break;
+				}
+				
+				case SDL_WINDOWEVENT_RESIZED:
+				{
+					// Resizing & moving panel
+					actionPanel.getOrigin().setX( Screen::get()->getWidth() - actionPanel.getWidth() );
+					actionPanel.setHeight( Screen::get()->getHeight() );
+					
+					// Moving buttons
+					for( vector<string>::iterator it = panelButtons.begin() ; it != panelButtons.end() ; it++ )
+						ui.getElement( *it )->getBox().getOrigin().setX( Screen::get()->getWidth() - ui.getElement( *it )->getBox().getWidth() - 10 );
+					
 					break;
 				}
 				
@@ -74,6 +102,12 @@ int main( int argc, char ** argv )
 		if( ticks - lastDrawTicks > 15 )
 		{		
 			Screen::get()->clear();
+			
+			Box actionPanelBorder( actionPanel );
+			actionPanelBorder.resizeBy( 1, 2 );
+			actionPanelBorder.moveBy( 0, -1 );
+			actionPanel.renderFill( Color( 0xEE, 0xEE, 0xEE, 128 ) );
+			actionPanel.render( Color( 0xAA, 0xAA, 0xAA, 128 ) );
 			
 			ui.render( ticks );
 					
