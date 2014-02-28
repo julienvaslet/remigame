c75ac055d4559042ee201f62b6cdc712f5c3c03b
Julien Vaslet <julien.vaslet@gmail.com>
2014-02-28 13:43:04 +0100

    Update ObjectEditor.cpp

--- apps/ObjectEditor.cpp
+++ apps/ObjectEditor.cpp
@@ -24,14 +24,28 @@ Point origin;
 Sprite * sprite = NULL;
 map<string, Animation *> animations;
 vector<string> animationsNames;
-string currentAnimation = "idle";
+int currentAnimation = 0;
 int currentZoom = 100;
 
-// User Interface
+// Global variables
 UserInterface editorUi;
+bool loadingState = false;
+bool saveingState = false;
 
 // Global functions
 void cleanObjectVariables();
+void synchronizeLabel( const string& name, const string& value );
+
+// Events
+bool decreaseZoom( Element * element );
+bool increaseZoom( Element * element );
+bool loadFile( Element * element );
+bool saveFile( Element * element );
+bool prevAnimation( Element * element );
+bool nextAnimation( Element * element );
+
+// TODO: add disabled state to ui::Element
+// TODO: add show state to ui::UserInterface's Element
 
 int main( int argc, char ** argv )
 {
@@ -67,22 +81,26 @@ int main( int argc, char ** argv )
 	editorUi.addElement( "btn_load", new Button( "font0", "Load" ) );
 	editorUi.getElement( "btn_load" )->getBox().setWidth( 137 );
 	editorUi.getElement( "btn_load" )->getBox().getOrigin().move( currentScreenWidth - 290, 310 );
+	editorUi.getElement( "btn_load" )->addEvent( "mouseup", loadFile );
 	panelButtons.push_back( "btn_load" );
 	
 	editorUi.addElement( "btn_save", new Button( "font0", "Save" ) );
 	editorUi.getElement( "btn_save" )->getBox().setWidth( 137 );
 	editorUi.getElement( "btn_save" )->getBox().getOrigin().move( currentScreenWidth - 147, 310 );
+	editorUi.getElement( "btn_save" )->addEvent( "mouseup", saveFile );
 	panelButtons.push_back( "btn_save" );
 	
 	// h: 340 - Animation selection
 	editorUi.addElement( "btn_prev_animation", new Button( "font0", "<" ) );
 	editorUi.getElement( "btn_prev_animation" )->getBox().setWidth( 40 );
 	editorUi.getElement( "btn_prev_animation" )->getBox().getOrigin().move( currentScreenWidth - 290 , 340 );
+	editorUi.getElement( "btn_prev_animation" )->addEvent( "mouseup", prevAnimation );
 	panelButtons.push_back( "btn_prev_animation" );
 	
 	editorUi.addElement( "btn_next_animation", new Button( "font0", ">" ) );
 	editorUi.getElement( "btn_next_animation" )->getBox().setWidth( 40 );
 	editorUi.getElement( "btn_next_animation" )->getBox().getOrigin().move( currentScreenWidth - 50, 340 );
+	editorUi.getElement( "btn_next_animation" )->addEvent( "mouseup", nextAnimation );
 	panelButtons.push_back( "btn_next_animation" );
 	
 	editorUi.addElement( "lbl_animation", new Label( "font0", "idle" ) );
@@ -220,17 +238,33 @@ int main( int argc, char ** argv )
 	editorUi.addElement( "btn_decrease_zoom", new Button( "font0", "~" ) );
 	editorUi.getElement( "btn_decrease_zoom" )->getBox().setWidth( 40 );
 	editorUi.getElement( "btn_decrease_zoom" )->getBox().getOrigin().move( currentScreenWidth - 290 , 570 );
+	editorUi.getElement( "btn_decrease_zoom" )->addEvent( "mouseup", decreaseZoom );
 	panelButtons.push_back( "btn_decrease_zoom" );
 	
 	editorUi.addElement( "btn_increase_zoom", new Button( "font0", "+" ) );
 	editorUi.getElement( "btn_increase_zoom" )->getBox().setWidth( 40 );
 	editorUi.getElement( "btn_increase_zoom" )->getBox().getOrigin().move( currentScreenWidth - 50, 570 );
+	editorUi.getElement( "btn_increase_zoom" )->addEvent( "mouseup", increaseZoom );
 	panelButtons.push_back( "btn_increase_zoom" );
 	
 	editorUi.addElement( "lbl_zoom", new Label( "font0", "100%" ) );
 	editorUi.getElement( "lbl_zoom" )->getBox().setWidth( 190 );
 	editorUi.getElement( "lbl_zoom" )->getBox().getOrigin().move( currentScreenWidth - 245, 570 );
 	panelButtons.push_back( "lbl_zoom" );
+	
+	editorUi.addElement( "lbl_animation", new Label( "font0", "idle" ) );
+	editorUi.getElement( "lbl_animation" )->getBox().setWidth( 190 );
+	editorUi.getElement( "lbl_animation" )->getBox().getOrigin().move( currentScreenWidth - 245, 340 );
+	panelButtons.push_back( "lbl_animation" );
+	
+	// Loading & saving status labels
+	editorUi.addElement( "lbl_loading", new Label( "font0", "Drop a XML or PNG file here" ) );
+	editorUi.getElement( "lbl_loading" )->getBox().resize( currentScreenWidth, Screen::get()->getHeight() );
+	panelButtons.push_back( "lbl_loading" );
+	
+	editorUi.addElement( "lbl_saving", new Label( "font0", "Saving the XML file..." ) );
+	editorUi.getElement( "lbl_saving" )->getBox().resize( currentScreenWidth, Screen::get()->getHeight() );
+	panelButtons.push_back( "lbl_saving" );
 	
 	while( running )
 	{
@@ -296,9 +330,13 @@ int main( int argc, char ** argv )
 		unsigned int ticks = SDL_GetTicks();
 		
 		if( ticks - lastDrawTicks > 15 )
-		{		
+		{
 			Screen::get()->clear();
 			
+			// Object rendering
+			
+			
+			// UserInterface rendering
 			Box actionPanelBorder( actionPanel );
 			actionPanelBorder.resizeBy( 1, 2 );
 			actionPanelBorder.getOrigin().moveBy( 0, -1 );
@@ -335,14 +373,67 @@ void cleanObjectVariables()
 		sprite = NULL;
 	}
 
-	currentAnimation = "idle";
-	Label * lblAnimation = reinterpret_cast<Label *>( editorUi.getElement( "lbl_animation" ) );
-	lblAnimation->setValue( currentAnimation );
+	currentAnimation = 0;
+	synchronizeLabel( "lbl_animation", animationNames[currentAnimation] );
 
 	currentZoom = 100;
 	stringstream ss;
-	ss << currentZoom;
-	Label * lblZoom = reinterpret_cast<Label *>( editorUi.getElement( "lbl_zoom" ) );
-	lblZoom->setValue( ss.str() );
+	ss << currentZoom << " %";
+	synchronizeLabel( "lbl_zoom", ss.str() );
 }
 
+void synchronizeLabel( const string& name, const string& value )
+{
+	Label * label = reinterpret_cast<Label *>( editorUi.getElement( name ) );
+	label->setValue( value );
+}
+
+bool decreaseZoom( Element * element )
+{
+	stringstream ss;
+	currentZoom -= 5;
+	ss << currentZoom << " %";
+	synchronizeLabel( "lbl_zoom", ss.str() );
+	return true;
+}
+
+bool increaseZoom( Element * element )
+{
+	stringstream ss;
+	currentZoom += 5;
+	ss << currentZoom << " %";
+	synchronizeLabel( "lbl_zoom", ss.str() );
+	return true;
+}
+
+bool loadFile( Element * element )
+{
+	return true;
+}
+
+bool saveFile( Element * element )
+{
+	return true;
+}
+
+bool prevAnimation( Element * element )
+{
+	currentAnimation--;
+	
+	if( currentAnimation < 0 )
+		currentAnimation = animationsNames.size() - 1;
+	
+	synchronizeLabel( "lbl_animation", animationNames[currentAnimation] );
+	return true;
+}
+
+bool nextAnimation( Element * element )
+{
+	currentAnimation++;
+	
+	if( currentAnimation >= animationsNames.size() )
+		currentAnimation = 0;
+		
+	synchronizeLabel( "lbl_animation", animationNames[currentAnimation] );
+	return true;
+}
